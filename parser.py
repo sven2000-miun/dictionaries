92997f71ce96d55f44981b161ee11ab66e58d1c5
sven <sven2000@student.miun.se>
2023-04-13 23:01:30 +0200

    Changed the script

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ remove = ["names.txt", "remove.txt", "countries.txt", "cities.txt"]
 banned = set()
 
 for each in remove:
-    with open(each, "r") as f:
+    with open("dicts/" + each, "r") as f:
         for each in f.readlines():
             banned.add(each)
 
@@ -13,5 +13,15 @@ with open("dicts/wordlist.10000", "r") as f:
         if each in banned:
             continue
         file.write(each)
+
+file.close()
+
+file = open("known_abbr.txt", "w")
+for abbr in ["abbr/it_abbr.txt", "abbr/eng_abbr.txt"]:
+    with open(abbr, "r") as f:
+        for each in f.readlines():
+            if each in banned:
+                continue
+            file.write(each)
 
 file.close()
